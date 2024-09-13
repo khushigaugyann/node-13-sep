b335d07ffaf19eafe192572b62ad10a94e71805e
khushigaugyann <khushisainigaugyan@gmail.com>
2024-09-13 12:50:41 +0530

    commit

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.get('/users', (req, res)=>{
     usercontrollers.getAllUsers(req, res);
 })
+router.get('/user/:id', (req, res)=>{
+    usercontrollers.getUser(req, res);
+})
 
 module.exports = router
